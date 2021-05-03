7cefe3ef006126be95f41998df9430863c067284
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-05-03 10:33:29 +0200

    V 0.02

--- scripts/product-preferences.ps1
+++ scripts/product-preferences.ps1
@@ -1,14 +1,21 @@
 #
-#  Initial version 0.01
+#  Version 0.02
 #
 set-strictMode -version latest
 
-$productPreferencesFile = "$env:appdata\SQL Developer\system19.2.1.247.2212\o.sqldeveloper\product-preferences.xml"
+$productPreferencesFile = get-childItem "$env:appdata\SQL Developer\system*\o.sqldeveloper\product-preferences.xml"
 
-if (-not (test-path $productPreferencesFile) ) {
-   write-host "$productPreferencesFile does not exist"
+if ($productPreferencesFile -eq $null) {
+   write-host "Product preference file does not exist or was not found."
    return
 }
+
+if ($productPreferencesFile -is [array]) {
+   write-host "Unable to determine product preference file uniquely"
+   return
+}
+
+write-host "product preferences file: $productPreferencesFile"
 
 copy-item $productPreferencesFile "$productPreferencesFile.bak"
 
@@ -32,24 +39,30 @@ if ($DBConfig -eq $null) {
 
 function set-DBConfig-value($n, $v) {
 
-  $node = $DBConfig.SelectSingleNode("value[@n='$n']")
+   $node = $DBConfig.SelectSingleNode("value[@n='$n']")
 
-  if ($node -eq $null) {
-     $node = $doc.CreateElement('value')
-     $node.SetAttribute('n', $n)
-     $null = $DBConfig.AppendChild($node)
-  }
+   if ($node -eq $null) {
+      $node = $doc.CreateElement('value')
+      $node.SetAttribute('n', $n)
+      $null = $DBConfig.AppendChild($node)
+   }
 
-  write-host "Changing $n from $($node.GetAttribute('v')) to $v"
+   write-host "Changing $n from $($node.GetAttribute('v')) to $v"
 
-  $node.SetAttribute('v', $v)
+   $node.SetAttribute('v', $v)
 }
 
-set-DBConfig-value ''               'C:\Users\Rene\github\Oracle\SQLPATH\login.sql'  # Startup script
-set-DBConfig-value 'NLS_DATE_FORM'  'yyyy-mm-dd hh24:mi:ss'
-set-DBConfig-value 'NLS_TS_FORM'    'yyyy-mm-dd hh24:mi:ssXff'
-set-DBConfig-value 'NLS_TS_TZ_FORM' 'yyyy-mm-dd hh24:mi:ssXff tzr'
-set-DBConfig-value 'NULLDISPLAY'    '-'
-set-DBConfig-value 'GLOGIN'         'true'
+# set-DBConfig-value ''                        'C:\Users\Rene\github\Oracle\SQLPATH\login.sql'  # Startup script
+# set-DBConfig-value 'DEFAULTPATH'             'C:\Users\Rene'
+  set-DBConfig-value 'NLS_DATE_FORM'           'yyyy-mm-dd hh24:mi:ss'
+  set-DBConfig-value 'NLS_TS_FORM'             'yyyy-mm-dd hh24:mi:ssXff'
+  set-DBConfig-value 'NLS_TS_TZ_FORM'          'yyyy-mm-dd hh24:mi:ssXff tzr'
+  set-DBConfig-value 'NULLDISPLAY'             '-'
+  set-DBConfig-value 'NULLCOLOR'               'NONE'
+  set-DBConfig-value 'GLOGIN'                  'true'
+# set-DBConfig-value 'KERBEROS_CONFIG'         'C:\Oracle\network\admin\krb5.conf'
+# set-DBConfig-value 'TNS_NAMES_DIR'           'C:\oracle\network\admin'
+# set-DBConfig-value 'UNSHAREDWORKSHEETOPEN'   'true'
+# set-DBConfig-value 'USE_THICK_DRIVER'        'true'
 
 $doc.Save($productPreferencesFile)
